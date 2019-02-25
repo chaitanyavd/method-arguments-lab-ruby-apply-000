fd733659570c6dcd6869ded5c88a393aaaddcd58
astonishing-glitch-8485 <chaitanya.v.de@gmail.com>
2019-02-25 03:35:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,3 +4,7 @@ def introduction(name)
 end
 
 puts introduction("CD")
+
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  end
